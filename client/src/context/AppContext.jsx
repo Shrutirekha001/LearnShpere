268992b3d7158999dc6e3b8611af8babb37e59b1
Shrutirekha001 <shrutipami2004@gmail.com>
2025-06-18 21:07:18 +0530

    update content

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,9 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import {useAuth, useUser} from "@clerk/clerk-react";
+
+
  export const AppContext = createContext()
 
  export const AppContextProvider = (props) => {
@@ -9,6 +12,9 @@ import humanizeDuration from "humanize-duration";
   const currency = import.meta.env.VITE_CURRENCY
 
   const navigate = useNavigate()
+
+  const {getToken} = useAuth()
+  const {user} = useUser()
 
 
   const [allCourses, setAllCourses] = useState([])
@@ -71,6 +77,17 @@ import humanizeDuration from "humanize-duration";
     fetchAllCourses()
     fetchUserEnrolledCourses()
   },[])
+
+  const logToken = async ()=>{
+    console.log(await getToken());
+  }
+
+
+  useEffect(()=>{
+    if(user){
+        logToken()
+    }
+  },[user])
 
 
    const value = {
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
 
 
 
@@ -14,11 +16,13 @@ await connectDB();
 
 // Middleware
 app.use(cors());
+app.use(clerkMiddleware())
 
 
 //Routes
 app.get('/', (req, res) => res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
 
 // port
 const PORT = process.env.PORT || 5000
